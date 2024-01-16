cbb6938c30f367ac65f5e39162fe827c46d86d8b
Juto21 <120532231+Juto21@users.noreply.github.com>
2024-01-16 19:58:36 +0700

    master

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -5,6 +5,7 @@ const MainMenu = ({ onStartClick, onQuitClick }) => {
   return (
     <div className="main-menu">
       <h1>Memory Matching Game (Animal) </h1>
+      <h2> 7C / Kelompok 1: Anggun Jevian ( 20670077) , Bagas Hary S ( 20670017) , Juanda Santoso Eka P (20670010) </h2>
       <button onClick={onStartClick}>Start</button>
       <button onClick={onQuitClick}>Quit</button>
     </div>
